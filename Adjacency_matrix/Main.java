9d29b52a14fa5c885092ec0cf72a042840a9b3a4
BoQasem <93032154+BoQasem@users.noreply.github.com>
2021-11-29 18:19:36 +0200

    Update Main.java

--- Adjacency_matrix/Main.java
+++ Adjacency_matrix/Main.java
@@ -12,7 +12,7 @@ public class Main {
 		
 		graph.addEdge(0, 1);
 		graph.addEdge(1, 2);
-        graph.addEdge(1, 4); 
+       		graph.addEdge(1, 4); 
 		graph.addEdge(2, 3);
 		graph.addEdge(2, 4);
 		graph.addEdge(4, 0);
